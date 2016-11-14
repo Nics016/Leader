e149d24c30f2ce233981b21513337985549d7339
Nikita Abrashnev <Nikita Abrashnev>
2016-11-15 02:31:37 +0300

    Initial

---
+++ ftp/footer.php
@@ -0,0 +1,15 @@
+<footer id="main-footer">
+	<div class="container">
+		<div class="footer-down clearfix">
+			<a href="#" class="footer-down-youtube"></a>
+			<a href="#" class="footer-down-instagram"></a>
+			<a href="#" class="footer-down-vk"></a>
+			<a href="#" class="footer-down-phone">8(800) 123 78 90</a>
+		</div>
+	</div>
+</footer>
+
+</div>
+	<!-- END OF MAIN-WRAP -->
+</body>
+</html>
---
+++ ftp/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="http://fond-lider.r01host.ru/wp-content/themes/LeaderTheme/css/style.css">
+	<link rel="stylesheet" href="http://fond-lider.r01host.ru/wp-content/themes/LeaderTheme/css/header.css">
+	<link rel="stylesheet" href="http://fond-lider.r01host.ru/wp-content/themes/LeaderTheme/css/footer.css">
+	<link rel="stylesheet" href="http://fond-lider.r01host.ru/wp-content/themes/LeaderTheme/css/main.css">
+	<link rel="stylesheet" href="http://fond-lider.r01host.ru/wp-content/themes/LeaderTheme/css/about.css">
+	<meta charset="UTF-8">
+	<title>Благотворительный фонд "Лидер"</title>
+</head>
+<body>
+	<!-- MAIN-WRAP -->
+	<div id="main-wrap">
+		<!-- HEADER -->
+		<header id="main-header">
+			<!-- HEADER-CONTAINER -->
+			<div class="container"> 
+				<div class="header-pictures clearfix">
+					<img src="http://fond-lider.r01host.ru/wp-content/themes/LeaderTheme/img/top-pic1.jpg" alt="" class="header-pictures-pic1">
+					<a href="#"><img src="http://fond-lider.r01host.ru/wp-content/themes/LeaderTheme/img/top-pic2.jpg" alt="" class="header-pictures-pic2"></a>
+					<img src="http://fond-lider.r01host.ru/wp-content/themes/LeaderTheme/img/top-pic3.jpg" alt="" class="header-pictures-pic3">
+				</div>
+				<div class="header-menu">
+					<?php
+			    		if ( function_exists( 'wp_nav_menu' ) )
+			        		wp_nav_menu( 
+						        array( 
+						        'theme_location' => 'top-menu',
+						        'fallback_cb'=> 'top_menu',
+						        'container' => 'ul',
+						        'menu_id' => 'top-menu',
+						        'menu_class' => 'nav') 
+							);
+					    else custom_menu();
+					?>
+				</div>
+			</div>
+			<!-- END OF HEADER-CONTAINER -->
+			<hr class="header-hr">
+		</header>
+		<!-- END OF HEADER -->
